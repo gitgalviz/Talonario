92c036615d61c18012400347a3f505cf56e4195e
Esneyder Galviz <119534032+gitgalviz@users.noreply.github.com>
2024-03-06 18:44:11 -0500

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <div class="cont_titulo">
       <h1 class="text_titulo">Talonario</h1>
     </div>
+    <div :class="Alerta()">
+      <div class="cont_alerta">
+        <p class="text_alerta">{{ alerta }}</p>
+      </div>
+    </div>
     <div class="container" v-if="RegistroTalonario">
       <div class="cont_form">
         <div class="form">
@@ -54,9 +59,6 @@
         <div class="cont_direccion">
           <input type="text" placeholder="Direccion" v-model="direccion">
         </div>
-        <div class="cont_identificacion">
-          <input type="text" placeholder="Identificacion" v-model="identificacion">
-        </div>
         <div class="cont_opciones_boleta">
           <p class="text_option">Pagar boleta?</p>
           <select v-model="selectedBoleta">
@@ -80,7 +82,7 @@
               <img src="../img/trofeo.png" class="img_trofeo">
             </div>
             <div class="cont_text_t">
-              <p class="text_trofeo">{{ premio }}</p>
+              <p class="text_trofeo">{{ premio_Verificado }}</p>
             </div>
           </div>
           <div class="cont_precio">
@@ -88,7 +90,7 @@
               <img src="../img/dolar.png" class="img_dolar">
             </div>
             <div class="cont_text_p">
-              <p class="text_precio">{{ Precio }}</p>
+              <p class="text_precio">{{ Precio_Verificado }}</p>
             </div>
           </div>
           <div class="cont_loteria">
@@ -96,7 +98,7 @@
               <img src="../img/museo.png" class="img_museo">
             </div>
             <div class="cont_text_l">
-              <p class="text_loteria">{{ selectedLoteria }}</p>
+              <p class="text_loteria">{{ selectedLoteria_Verificado }}</p>
             </div>
           </div>
           <div class="cont_calendario">
@@ -104,7 +106,7 @@
               <img src="../img/calendario.png" class="img_calendario">
             </div>
             <div class="cont_text_c">
-              <p class="text_calendario">{{ fecha }}</p>
+              <p class="text_calendario">{{ fecha_Verificado }}</p>
             </div>
           </div>
           <div class="cont_btn_editar">
@@ -113,7 +115,7 @@
         </div>
       </div>
       <div class="conte_balotas">
-        <div v-for="(boleta, i) in boleta" :key="i">
+        <div v-for="(boleta, i) in boleta" :key="i" :class="getBoletaColor(boleta.estado)">
           <div class="cont_balota" @click="comprarBoleta(i)">
             <div class="cont_num_balota">
               <p class="text_num_balota">{{ boleta.item }}</p>
@@ -133,7 +135,7 @@
             <button class="btn2" @click="listarBoletas()">Listar tus boletas</button>
           </div>
           <div class="cont_btn3">
-            <button class="btn3" @click="personalizar()">Personalizar talonario</button>
+            <button class="btn3" @click="personalizarColores()">Personalizar talonario</button>
           </div>
           <div class="cont_btn4">
             <button class="btn4" @click="imprimir()">Generar PDF</button>
@@ -149,7 +151,6 @@
             <th>Nombre</th>
             <th>Telefono</th>
             <th>Direccion</th>
-            <th>Identificacion</th>
             <th>Estado de pago</th>
           </tr>
           <tr class="balotas_compradas" v-for="(boletasCompradas, i) in boletasCompradas" :key="i">
@@ -157,11 +158,49 @@
             <td>{{ boletasCompradas.nombre }}</td>
             <td>{{ boletasCompradas.telefono }}</td>
             <td>{{ boletasCompradas.direccion }}</td>
-            <td>{{ boletasCompradas.identificacion }}</td>
             <td>{{ boletasCompradas.pago }}</td>
           </tr>
         </table>
         <button class="button_atras" @click="atras()">Regresar</button>
+      </div>
+    </div>
+    <div class="cont_options_boletas" v-if="cont_options_boletas">
+      <div class="options_boletas">
+        <div class="cont_btn_datos">
+          <button @click="DatosDueño()">Información dueño</button>
+        </div>
+        <div class="cont_btn_pagar" v-if="pagarBoleta">
+          <button @click="PagarBoleta()">Pagar boleta</button>
+        </div>
+        <div class="cont_btn_liberar">
+          <button @click="liberarBalota()">Liberar balota</button>
+        </div>
+        <div class="cont_btn_atras">
+          <button @click="atras2()">Regresar</button>
+        </div>
+      </div>
+    </div>
+    <div class="cont_datos_dueño" v-if="cont_datos_dueño">
+      <div class="datos_dueño">
+        <div class="info_nombre">
+          <p class="text_info_nombre">Nombre:</p>
+          <p class="text_nombre">{{ dueño.nombre }}</p>
+        </div>
+        <div class="info_telefono">
+          <p class="text_info_telefono">Telefono:</p>
+          <p class="text_telefono">{{ dueño.telefono }}</p>
+        </div>
+        <div class="info_direccion">
+          <p class="text_info_direccion">Direccion:</p>
+          <p class="text_direccion">{{ dueño.direccion }}</p>
+        </div>
+        <div class="info_pago">
+          <p class="text_info_pago">Estado</p>
+          <p class="text_pago">{{ dueño.pago }}</p>
+        </div>
+        <div class="cont_btn_atras">
+          <button @click="atras3()">Regresar</button>
+        </div>
       </div>
     </div>
   </div>
@@ -172,77 +211,205 @@ import { ref } from 'vue';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
 
-
-const atras = () => {
-  balotasCompradas.value = false;
-  cuerpo.value = true;
-};
-
-
-
 jsPDF.autoTable = autoTable;
 
 const premio = ref('');
+const premio_Verificado = ref('');
 const Precio = ref('');
+const Precio_Verificado = ref('');
 const selectedLoteria = ref('');
+const selectedLoteria_Verificado = ref('');
 const selectedCantidad = ref('');
+const selectedBoleta = ref('');
 const fecha = ref('');
+const fecha_Verificado = ref('');
 const nombre = ref('');
 const telefono = ref('');
 const direccion = ref('');
-const identificacion = ref('');
 const pago = ref('');
 const RegistroTalonario = ref(true);
 const RegistrarDueño = ref(false);
 const cuerpo = ref(false);
 const boletaSeleccionada = ref(null);
 const balotasCompradas = ref(false);
+const estado = ref(0);
+const cont_options_boletas = ref(false);
+const pagarBoleta = ref(false);
+const estadoBoleta = ref('');
+const alerta = ref('');
+const cont_datos_dueño = ref(false);
+const id = ref(0);
+let i = ref(0);
 
 const boleta = ref([]);
 const boletasCompradas = ref([]);
+const dueño = ref([]);
+
+const ocultarAlerta = () => {
+  setTimeout(() => {
+    alerta.value = '';
+    Alerta();
+  }, 2500);
+};
+
+const Alerta = () => {
+  if (alerta.value !== '') {
+    return 'alerta2';
+  }
+  else {
+    return 'alerta';
+  }
+};
 
 const crearTalonario = () => {
-  const cantidad = selectedCantidad.value;
-
-  for (let i = 0; i < cantidad; i++) {
-    boleta.value.push({
-      item: i + 1,
-    });
+  if(premio.value === '' || Precio.value === '' || selectedLoteria.value === '' || selectedCantidad.value === '' || fecha.value === '') {
+    alerta.value = 'Todos los campos son obligatorios';
+    ocultarAlerta();
+    return;
   }
-  RegistroTalonario.value = false;
-  cuerpo.value = true;
+  else {
+    premio_Verificado.value = premio.value;
+    Precio_Verificado.value = Precio.value;
+    selectedLoteria_Verificado.value = selectedLoteria.value;
+    fecha_Verificado.value = fecha.value;
+    const cantidad = selectedCantidad.value;
+  
+    for (let i = 0; i < cantidad; i++) {
+      boleta.value.push({
+        item: i + 1,
+        estado: estado.value,
+      });
+    }
+    RegistroTalonario.value = false;
+    cuerpo.value = true;
+  }
 };
 
 const comprarBoleta = (index) => {
   boletaSeleccionada.value = index;
-  RegistrarDueño.value = true;
-  cuerpo.value = false;
+  if (boleta.value[index].estado === 0) {
+    RegistrarDueño.value = true;
+    cuerpo.value = false;
+  }
+  else if (boleta.value[index].estado === 1) {
+    cont_options_boletas.value = true;
+    pagarBoleta.value = false;
+    cuerpo.value = false;
+    i = boletasCompradas.value.findIndex((boletaComprada) => {
+      return boletaComprada.numero === boleta.value[index].item;
+    });
+  }
+  else if (boleta.value[index].estado === 2) {
+    cont_options_boletas.value = true;
+    pagarBoleta.value = true;
+    cuerpo.value = false;
+    i = boletasCompradas.value.findIndex((boletaComprada) => {
+      return boletaComprada.numero === boleta.value[index].item;
+    });
+  }
 };
 
 const registrar = () => {
-  RegistrarDueño.value = false;
+  if (nombre.value === '' || telefono.value === '' || direccion.value === '' || selectedBoleta.value === '') {
+    alerta.value = 'Todos los campos son obligatorios';
+    ocultarAlerta();
+    return;
+  }
+  else{
+    if (selectedBoleta.value === 'Si') {
+      estado.value = 1;
+      estadoBoleta.value = "Pagada";
+    }
+    else {
+      estado.value = 2;
+      estadoBoleta.value = "No pagada";
+    }
+    boletasCompradas.value.push({
+      id: id.value++,
+      numero: boleta.value[boletaSeleccionada.value].item,
+      nombre: nombre.value,
+      telefono: telefono.value,
+      direccion: direccion.value,
+      pago: estadoBoleta.value,
+    });
+    boleta.value[boletaSeleccionada.value].estado = estado.value;
+    nombre.value = '';
+    telefono.value = '';
+    direccion.value = '';
+    pago.value = '';
+    selectedBoleta.value = '';
+    estado.value = 0;
+    RegistrarDueño.value = false;
+    cuerpo.value = true;
+  }
+};
+
+const DatosDueño = () => {
+  cont_datos_dueño.value = true;
+  cont_options_boletas.value = false;
+  cuerpo.value = false;
+
+  dueño.value = {
+    nombre: boletasCompradas.value[i].nombre,
+    telefono: boletasCompradas.value[i].telefono,
+    direccion: boletasCompradas.value[i].direccion,
+    pago: boletasCompradas.value[i].pago,
+  };
+};
+
+
+const PagarBoleta = () => {
+  boleta.value[boletaSeleccionada.value].estado = 1;
+  cont_options_boletas.value = false;
+  cuerpo.value = true;
+};
+
+const liberarBalota = () => {
+  const selectedBoletaIndex = boletaSeleccionada.value;
+  boleta.value[selectedBoletaIndex].estado = 0;
+  cont_options_boletas.value = false;
   cuerpo.value = true;
 
-  boletasCompradas.value.push({
-    numero: boleta.value[boletaSeleccionada.value].item,
-    nombre: nombre.value,
-    telefono: telefono.value,
-    direccion: direccion.value,
-    identificacion: identificacion.value,
-    pago: pago.value.search(boletasCompradas.value) > -1 ? 'Paga' : 'No paga',
+  boletasCompradas.value = boletasCompradas.value.filter((boletaComprada) => {
+    return boletaComprada.numero !== boleta.value[selectedBoletaIndex].item;
   });
+};
 
-  nombre.value = '';
-  telefono.value = '';
-  direccion.value = '';
-  identificacion.value = '';
-  pago.value = '';
+const atras = () => {
+  balotasCompradas.value = false;
+  cuerpo.value = true;
+};  
 
+const atras2 = () => {
+  cont_options_boletas.value = false;
+  cuerpo.value = true;
+};
+
+const atras3 = () => {
+  cont_datos_dueño.value = false;
+  cuerpo.value = true;
+};
+
+const getBoletaColor = (estado) => {
+  switch (estado) {
+    case 0:
+      return 'ClaseInicial';
+    case 1:
+      return 'ClasePagada';
+    case 2:
+      return 'ClaseNoPagada';
+  }
 };
 
 const editarTalonario = () => {
+  premio.value = '';
+  Precio.value = '';
+  selectedLoteria.value = '';
+  selectedCantidad.value = '';
+  fecha.value = '';
   RegistroTalonario.value = true;
   cuerpo.value = false;
+  boleta.value = [];
 };
 
 const listarBoletas = () => {
@@ -250,32 +417,34 @@ const listarBoletas = () => {
   cuerpo.value = false;
 };
 
+const personalizarColores = () => {
+};
+
 const imprimir = () => {
   const doc = new jsPDF();
 
   doc.setFontSize(12);
-  doc.text("Resumen de boletas vendias", 10, 10);
+  doc.text("Resumen de boletas vendidas", 10, 10);
 
   const tableData = boletasCompradas.value.map((boleta, index) => [
     boleta.numero,
     boleta.nombre,
     boleta.telefono,
     boleta.direccion,
-    boleta.identificacion,
     boleta.pago,
-
   ]);
 
   doc.autoTable({
-    head: [["Boleta", "Nombre", "Teléfono", "Dirección", "Identificación", "Pago"]],
+    head: [["Boleta", "Nombre", "Teléfono", "Dirección", "Pago"]],
     body: tableData,
     startY: 20,
   });
 
+  const totalBoletas = boletasCompradas.value.length;
+  doc.text(`Total de balotas compradas: ${totalBoletas}`, 10, doc.autoTable.previous.finalY + 10);
+
   doc.save("vendidas.pdf");
-
 };
-
 </script>
 
 <style scoped>
@@ -295,6 +464,34 @@ const imprimir = () => {
   font-size: 50px;
   font-family: Arial;
   color: rgb(0, 0, 0);
+}
+
+.alerta {
+  display: none;
+}
+
+.alerta2{
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #00000095;
+}
+
+.cont_alerta {
+  width: 30%;
+  height: 25vh;
+  background-color: #ccc;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  font-weight: bold;
+  padding: 8px 1%;
+  text-align: center;
 }
 
 .cont_form_registro {
@@ -549,26 +746,10 @@ input[type="text"] {
   grid-template-columns: repeat(10, 1fr);
 }
 
-.cont_balota {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  background-color: #007bff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 5px;
-  cursor: pointer;
-}
-
 .text_num_balota {
   font-size: 16px;
   color: #fff;
   margin: 0;
-}
-
-.cont_balota:hover {
-  transform: scale(1.1);
 }
 
 /* diseño de tablas de balotas */
@@ -613,5 +794,128 @@ input[type="text"] {
 
 .button_atras:hover {
   background-color: #45a049;
+}
+
+.ClaseInicial {
+  background-color: #007bff;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  cursor: pointer;
+}
+
+.ClasePagada {
+  background-color: #28a745;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  cursor: pointer;
+}
+
+.ClaseNoPagada {
+  background-color: #dc3545;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  cursor: pointer;
+}
+
+.ClaseInicial:hover,
+.ClasePagada:hover,
+.ClaseNoPagada:hover {
+  transform: scale(1.1);
+}
+
+.cont_options_boletas {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  width: 100%;
+}
+
+.options_boletas {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ccc;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+.cont_btn_datos,
+.cont_btn_pagar,
+.cont_btn_liberar {
+  margin-bottom: 15px;
+}
+
+.cont_btn_datos button,
+.cont_btn_pagar button,
+.cont_btn_liberar button,
+.cont_btn_atras button {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: #ffffff;
+  color: #000000;
+  width: 200px;
+}
+
+.cont_btn_datos button:hover,
+.cont_btn_pagar button:hover,
+.cont_btn_liberar button:hover,
+.cont_btn_atras button:hover {
+  background-color: #1aff35;
+} 
+
+.cont_datos_dueño {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  width: 100%;
+}
+
+.datos_dueño {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ccc;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+.info_nombre,
+.info_telefono,
+.info_direccion,
+.info_pago {
+  margin-bottom: 15px;
+  display: flex;
+}
+
+.text_info_nombre,
+.text_info_telefono,
+.text_info_direccion,
+.text_info_pago {
+  font-size: 20px;
+  font-weight: bold;
+  margin-right: 10px;
 }
 </style>
